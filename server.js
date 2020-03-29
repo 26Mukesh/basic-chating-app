e4db95f2189e78332ff4e3d6dc8aada94b566484
26Mukesh <smukeshkumar240@gmail.com>
2020-03-29 07:59:11 -0700

    firstcommit

---
+++ server.js
@@ -0,0 +1,49 @@
+var mongo = require('mongodb').MongoClient;
+const client = require('socket.io').listen(4000).sockets
+var mongoURI ="mongodb+srv://mukesh:3unhleGDfRLBlJhn@cluster0-05i2g.mongodb.net/test?retryWrites=true&w=majority/chatingapp"
+
+mongo.connect(mongoURI, function(err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+
+    client.on('connection', function(socket){
+        let chat = db.collection('chats');
+        
+        //create function to send status
+        sendStatus = function(s) {
+            socket.emit('status',s)
+        }
+        //get chat from mongodb
+        chat.find().limit(100).sort({_id}).toArray(function(err, res){
+            if(err) throw err;
+            //emit message
+            socket.emit('output',res)
+        })
+        //handle input events
+        socket.on('input',function(data){
+            let name = data.name;
+            let msg = data.message
+
+            if(name =='' || message == ''){
+                sendStatus('please enter a name and message')
+            } else {
+                chat.insert({name: name, message: msg}, function(){
+                    client.emit('output', [data])
+                    sendStatus({
+                        message: 'message sent',
+                        clear: true
+                    })
+                })
+            }
+        })
+
+        //handle clear
+        socket.on('clear', function(){
+            chat.remove({},function(){
+                socket.emit('cleared')
+            })
+        })
+
+    })
+
+});
